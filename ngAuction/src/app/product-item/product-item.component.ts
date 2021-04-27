9f5067acad41dd5349b5abd6f557d66b3b9d4b4b
EunsangJeon <eunsang.jeon.127@gmail.com>
2021-04-27 16:15:15 +0900

    product service and product item elements

--- ngAuction/src/app/product-item/product-item.component.ts
+++ ngAuction/src/app/product-item/product-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../shared/product.service';
 
 @Component({
   selector: 'nga-product-item',
@@ -7,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductItemComponent implements OnInit {
 
+  @Input() product?: Product;
+
   constructor() {
   }
 
   ngOnInit(): void {
   }
-
 }

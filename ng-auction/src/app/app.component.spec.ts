c589ad5dc8b00defb27a3a932e88dd1f58ff53eb
EunsangJeon <eunsang.jeon.127@gmail.com>
2021-04-29 19:13:02 +0900

    style and theme

--- ng-auction/src/app/app.component.spec.ts
+++ ng-auction/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'ng-auction'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-auction');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-auction app is running!');
-  });
 });
